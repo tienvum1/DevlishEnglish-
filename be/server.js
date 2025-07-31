668122a2c66c29d40e3d0871c3039b1ed722bad4
ablue <tienvukk1@gmail.com>
2025-07-31 16:07:29 +0700

    add server.sj

---
+++ be/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4999;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
